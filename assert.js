4f4f0dae942aca1075dbecd07f5f3ada694c2281
Chris Mitchell <chrisjmit@gmail.com>
2017-05-22 14:43:58 +0100

    Assert test added for falsy

--- assert.js
+++ assert.js
@@ -1,7 +1,15 @@
 var assert = {
+
   isTrue: function(assertionToCheck) {
     if (!assertionToCheck) {
       throw new Error("Assertion failed: " + assertionToCheck + " is not truthy");
     }
-  }
+  },
+
+  isFalse: function(assertionToCheck) {
+    if (!!assertionToCheck) {
+      throw new Error("Assertion failed: " + assertionToCheck + " is not falsy");
+    }
+  },
+
 };
