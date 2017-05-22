54797f9bb36caf7706d5de843b03b87bb96f90d8
Chris Mitchell <chrisjmit@gmail.com>
2017-05-22 14:36:59 +0100

    Assert test library item added

--- note-test.js
+++ note-test.js
@@ -5,7 +5,15 @@
     if (note.text !== "test") {
       throw new Error("Note requires text argument");
     }
-  };
+  }
 
   testNoteInitializesWithText();
 })(this);
+
+
+function testNoteInitializesWithText(){
+  var note = new Note("Remember to remember");
+  assert.isTrue(note.text === "Remember to remember");
+}
+
+testNoteInitializesWithText();
