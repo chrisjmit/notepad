13d65ec73382adb91970ae83fbb39fe85596a76c
Chris Mitchell <chrisjmit@gmail.com>
2017-05-23 15:12:10 +0100

    Test added for array to contain list and note

--- note-list-view.js
+++ note-list-view.js
@@ -1,7 +1,13 @@
 (function(exports) {
-  function View() {
+  function View(list) {
+    this._lists = [];
 
-  }
+}
+
+
+  View.prototype.getLists = function(){
+    return this._lists;
+  };
 
   exports.View = View;
 })(this);
--- spec/noteListViewSpec.js
+++ spec/noteListViewSpec.js
@@ -4,3 +4,18 @@ function testInstantiatesNoteListView() {
 }
 
 testInstantiatesNoteListView();
+
+function testViewCanStoreList() {
+  var view = new View();
+  assert.isArray(view._lists);
+}
+
+testViewCanStoreList();
+
+function testViewContainsListNote(){
+  var list = new List();
+  var view = new View(list);
+  assert.isEq(view.getLists(), [[Note]]);
+}
+
+testViewContainsListNote();
